5481a02c4a4f4bbf0cf9d4216f77763f925f0dfb
veldhi Akashreddy <veldhiakashreddy@gmail.com>
2026-02-20 23:41:31 +0530

    Add files via upload

---
+++ encapsulation.java
@@ -0,0 +1,29 @@
+package oops230126;
+
+public class encapsulation {
+	    private int age;
+	    private String name;
+
+	    // getter
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    // setter
+	    public void setAge(int a) {
+	        age = a;
+	    }
+
+	    public void setName(String b) {
+	    name = b;
+	    }
+	}
+
+	    
+
+
+
